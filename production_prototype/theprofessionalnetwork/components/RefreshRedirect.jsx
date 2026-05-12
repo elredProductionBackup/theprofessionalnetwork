bce41302bd15b39a335644d357c402a5f758cf33
elredProductionBackup <productionbackup@elred.io>
2026-05-12 13:56:34 +0530

    new

---
+++ production_prototype/theprofessionalnetwork/components/RefreshRedirect.jsx
@@ -0,0 +1,18 @@
+"use client";
+import { useEffect } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
+
+export default function RefreshRedirect() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname !== '/') {
+      router.replace('/');
+    }
+    // Force scroll to top on every load/refresh
+    window.scrollTo(0, 0);
+  }, []);
+
+  return null;
+}
